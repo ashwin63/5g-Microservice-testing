bd2a30581b08349d6d6792b2fb79c50282906b08
ashwin63 <ashwinsenthil63@gmail.com>
2024-06-24 11:16:27 -0400

    Create restler_radamsa.sh

---
+++ restler_radamsa.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Execute the restler/Restler test command
+echo "Restler Compile started" >> /RESTler/restler_log
+/RESTler/restler/Restler compile --api_spec /RESTler/json/swagger.json 
+/RESTler/restler/Restler test --grammar_file /RESTler/Compile/grammar.py --dictionary_file /RESTler/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
+
+#echo "Restler Test Passed" >> /RESTler/restler_log
+# Check if the command was successful
+
+if [ $? -eq 0 ]; then
+    # If successful, execute the restler/Restler fuzz command
+    echo "Restler Fuzz started" >> /RESTler/restler_log
+    /RESTler/restler/Restler fuzz --grammar_file /RESTler/Compile/grammar.py --dictionary_file /RESTler/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
+    echo "Restler Fuzz Finished" >> /RESTler/restler_log
+else
+    # If the test command failed, enter an infinite loop
+    echo "Restler Test failed" >> /RESTler/restler_log
+    while true; do
+        sleep 1
+    done
+fi
