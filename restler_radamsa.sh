f646f4099aec583476921f54d1933e8a48f4828b
ashwin63 <ashwinsenthil63@gmail.com>
2024-06-26 10:27:27 -0400

    Added flag -r => this uses settings file to include radamsa

--- restler_radamsa.sh
+++ restler_radamsa.sh
@@ -5,6 +5,12 @@ filename="swagger.json"
 if [ $# -gt 0 ]; then
     filename="$1"
 fi
+radamsa = false
+if [ $# -gt 1 && ]; then
+    if [$2 == '-r']; then
+        radamsa = true
+    fi
+fi
 #install radamsa
 apk add make
 apk add gcc
@@ -18,15 +24,21 @@ cd /RESTler/json
 # Execute the restler/Restler test command
 echo "Restler Compile started" >> /RESTler/json/restler_log
 /RESTler/restler/Restler compile --api_spec /RESTler/json/"$filename" 
-/RESTler/restler/Restler test --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
-
+if [ radamsa == true]; then 
+    /RESTler/restler/Restler test --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
+else
+    /RESTler/restler/Restler test --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --no_ssl
+fi
 #echo "Restler Test Passed" >> /RESTler/restler_log
 # Check if the command was successful
 
 if [ $? -eq 0 ]; then
     # If successful, execute the restler/Restler fuzz command
     echo "Restler Fuzz started" >> /RESTler/json/restler_log
-    /RESTler/restler/Restler fuzz --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
+    if [ radamsa == true]; then
+        /RESTler/restler/Restler fuzz --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
+    else
+        /RESTler/restler/Restler fuzz --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --no_ssl
     echo "Restler Fuzz Finished" >> /RESTler/json/restler_log
 else
     # If the test command failed, enter an infinite loop
