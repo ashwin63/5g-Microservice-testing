80bee0c672d8919d14036790cae05c2c474c832e
ashwin63 <ashwinsenthil63@gmail.com>
2024-07-11 10:45:11 -0400

    Update restler.sh

--- restler.sh
+++ restler.sh
@@ -2,14 +2,10 @@
 # Define the default filename
 # first argument filename 
 # -r => uses radamsa
+# -i => reinstalls restler
 filename="swagger.json"
 radamsa=false
-apk add dotnet6-sdk
-mkdir RESTLER_modified
-cd RESTLER_modified
-git clone https://github.com/ashwin63/restler-fuzzer.git
-mkdir restler_bin
-python ./restler-fuzzer/build-restler.py --dest_dir /RESTler/json/RESTLER_modified/restler_bin
+
 # Check for command line arguments and assign to filename if present
 if [ $# -gt 0 ]; then
     filename="$1"
@@ -20,6 +16,14 @@ fi
 if [ "$2" == '-r' ]; then
     radamsa=true
     echo "Radamsa: $radamsa"
+fi
+if [ "$3" == '-i' ]; then
+    apk add dotnet6-sdk
+    mkdir RESTLER_modified
+    cd RESTLER_modified
+    git clone https://github.com/ashwin63/restler-fuzzer.git
+    mkdir restler_bin
+    python ./restler-fuzzer/build-restler.py --dest_dir /RESTler/json/RESTLER_modified/restler_bin
 fi
 #install radamsa
 if $radamsa
@@ -33,15 +37,15 @@ then
     make
     make install
 fi
-cd /RESTler/json
+cd /RESTler/json/RESTLER_modified/
 # Execute the restler/Restler test command
-echo "Restler Compile started" >> /RESTler/json/restler_log
-/RESTler/restler/Restler compile --api_spec /RESTler/json/"$filename" 
+echo "Restler Compile started" >> /RESTler/json/RESTLER_modified/restler_log
+/RESTler/json/RESTLER_modified/restler_bin/restler/Restler compile --api_spec /RESTler/json/"$filename" 
 if $radamsa 
 then 
-    /RESTler/restler/Restler test --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
+    /RESTler/json/RESTLER_modified/restler_bin/restler/Restler test --grammar_file /RESTler/json/RESTLER_modified/Compile/grammar.py --dictionary_file  /RESTler/json/RESTLER_modified/Compile/dict.json --settings  /RESTler/json/settings.json --no_ssl
 else
-    /RESTler/restler/Restler test --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --no_ssl
+    /RESTler/json/RESTLER_modified/restler_bin/restler/Restler test --grammar_file  /RESTler/json/RESTLER_modified/Compile/grammar.py --dictionary_file  /RESTler/json/RESTLER_modified/Compile/dict.json --no_ssl
 fi
 #echo "Restler Test Passed" >> /RESTler/restler_log
 # Check if the command was successful
@@ -52,10 +56,10 @@ if [ $? -eq 0 ]; then
     if $radamsa 
     then
         echo "using radamsa"
-        /RESTler/restler/Restler fuzz --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
+         /RESTler/json/RESTLER_modified/restler_bin/restler/Restler  fuzz --grammar_file  /RESTler/json/RESTLER_modified/Compile/grammar.py --dictionary_file   /RESTler/json/RESTLER_modified/Compile/dict.json --settings /RESTler/json/settings.json --no_ssl
     else
         echo " Starting fuzzing with default dictionary"
-        /RESTler/restler/Restler fuzz --grammar_file /RESTler/json/Compile/grammar.py --dictionary_file /RESTler/json/Compile/dict.json --no_ssl
+         /RESTler/json/RESTLER_modified/restler_bin/restler/Restler  fuzz --grammar_file  /RESTler/json/RESTLER_modified/Compile/grammar.py --dictionary_file  /RESTler/json/RESTLER_modified/dict.json --no_ssl
     fi
     echo "Restler Fuzz Finished" >> /RESTler/json/restler_log
 else
